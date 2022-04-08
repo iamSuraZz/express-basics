20dfc2b4990fb30f660051f317b628df894423fb
SuraZz007 <surajdev9886@gmail.com>
2022-04-08 18:07:17 +0530

    add json

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/secretdetails", (req, res) => {
+  res.json({
+    name: "Suraj",
+    lastname: "Poddar",
+    number: 9815379803,
+  });
+});
+
+app.get("/bootstrap", (req, res) => {
+  res.sendFile(path.join(__dirname, "bootstrap.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
